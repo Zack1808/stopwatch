499d210148d8c4a95f3a84710e1a2dcb83e5f5d4
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-11 09:11:10 +0200

    Recreated the src folder, the index file and the App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Importing the style file
+import '../css/App.css';
+
+// Creating the App component
+const App = () => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App;
